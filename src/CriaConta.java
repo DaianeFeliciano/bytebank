f1cc23d09260d905514c41a50a2c9765fd1c8942
Daiane Feliciano <77757301+DaianeFeliciano@users.noreply.github.com>
2021-08-11 16:07:44 -0300

    classe CriaConta

--- src/CriaConta.java
+++ src/CriaConta.java
@@ -1,10 +1,19 @@
 public class CriaConta {
     public static void main(String[] args) {
         Conta primeiraConta = new Conta ();
+        Conta segundaConta = new Conta ();
 
         primeiraConta.saldo = 200;
 
         System.out.println(primeiraConta.saldo);
 
+        primeiraConta.saldo += 100;
+
+        System.out.println(primeiraConta.saldo);
+
+        segundaConta.saldo = 200;
+
+        System.out.println("Saldo da primeira conta: " + primeiraConta.saldo);
+        System.out.println("Saldo da segunda conta: " + segundaConta.saldo);
     }
 }
