c9a473dd9d7add5240135988891427c68968d061
Daiane Feliciano <77757301+DaianeFeliciano@users.noreply.github.com>
2021-08-11 17:21:23 -0300

    metodo transfere

--- src/Conta.java
+++ src/Conta.java
@@ -4,13 +4,13 @@ public class Conta {
     int agencia;
     int numero;
     String titular;
-
+// metodo sem retorno
     public void deposita(double valor) {
 
         this.saldo += valor;
 
     }
-
+// metodo com retorno
     public boolean saca(double valor) {
         if(this.saldo >= valor) {
         this.saldo -= valor;
@@ -20,6 +20,17 @@ public class Conta {
         return false;
 
         }
+    }
+
+    public boolean transfere(double valor, Conta destino) {
+        if(this.saldo >=valor) {
+
+            this.saldo -= valor;
+            destino.deposita(valor);
+            return true;
+        }
+        return false;
+
     }
 
 }
--- src/TestaMetodo.java
+++ src/TestaMetodo.java
@@ -2,6 +2,7 @@ public class TestaMetodo {
 
     public static void main(String[] args) {
         Conta contaDaDaiane = new Conta();
+        Conta contaDoVictor = new Conta();
 
         contaDaDaiane.saldo = 100;
         contaDaDaiane.deposita(50); // altera o valor, metodo
@@ -12,6 +13,24 @@ public class TestaMetodo {
 
         System.out.println(contaDaDaiane.saldo);
         System.out.println(conseguiuRetirar);
+
+        contaDoVictor.deposita(1000);
+
+        boolean sucessoTransferencia = contaDoVictor.transfere(100, contaDaDaiane);
+
+        //boolean sucessoTransferencia2 = contaDoVictor.transfere(0, contaDaDaiane);
+
+        if(sucessoTransferencia){
+
+            System.out.println("Transferencia feita com sucesso");
+        } else {
+
+            System.out.println("Saldo insuficiente");
+        }
+        System.out.println(contaDaDaiane.saldo);
+        System.out.println(contaDoVictor.saldo);
+
+
     }
 
 }
